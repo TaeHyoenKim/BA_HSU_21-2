56a59721e45d639b483220af371d915aec03635d
TaeHyeon_Kim <thk98k@naver.com>
2021-11-20 20:54:38 +0900

    temperature, humidity, windspeed, dew point temperature, solar radiation 에 대한 다중 회귀분석

--- 1731181_김태현_FinalProject.R
+++ 1731181_김태현_FinalProject.R
@@ -6,6 +6,10 @@ View(bike)
 str(bike) # 8769개 행, 14개 열
 summary(bike)
 
+# 자전거를 가장 많이 대여한 날은?
+bike$Date[which.max(bike$Rented.Bike.Count)]
+
+
 ### 데이터 전처리 ###
 # 변수 이름 변경
 names(bike)[4] = 'Temperature'
@@ -16,19 +20,18 @@ names(bike)[8] = 'Dew.point.temperature'
 names(bike)[9] = 'Solar.Radiation'
 names(bike)[10] = 'Rainfall'
 names(bike)[11] = 'Snowfall' 
-names(bike)
+# names(bike)
 
 # 변수 삭제
 bike = bike[bike$Functioning.Day == 'Yes', ]
 bike$Functioning.Day = NULL #필요없는 변수 삭제
-
 
 ### RentedBikeCount - Temperature 관계 파악 ###
 windows()
 plot(bike$Temperature, bike$Rented.Bike.Count) #일반적으로 온도가 높아질수록 대여량이 늘어나는 형태 
 
 
-### Modeling _ 선형 회귀분석 ###
+### Modeling _ 단순 선형 회귀분석 ###
 # y = ax + b
 # y:(RentedBikeCount), x:(Temperature), (a,b): parameter
 bike.temp.reg = lm(Rented.Bike.Count ~ Temperature, data = bike)
@@ -50,3 +53,18 @@ summary(bike.temp.reg)
 
 # 온도가 1도 올라가면?
 # 온도가 1도 올라갈 때마다 대여량은 29대 증가
+
+
+### Modeling _ 다중 선형 회귀분석 ###
+names(bike)
+bike.all.reg = lm(Rented.Bike.Count ~ Temperature + Humidity + Wind.speed + Visibility + Dew.point.temperature + Solar.Radiation, data = bike)
+# bike.all.reg = lm(Rented.Bike.Count ~ Temperature + Humidity + Wind.speed + Visibility, data = bike)
+
+summary(bike.all.reg)
+# p-value 값 유의
+# Visibility, Dew.point.temperature 유의하지 않음
+bike.all.reg2 = lm(Rented.Bike.Count ~ Temperature + Humidity + Wind.speed + Solar.Radiation, data = bike)
+summary(bike.all.reg2)
+# Rented.Bike.Count = 905.097 + 36.243*Temperature -11.333*Humidity + 50.743*Wind.speed -118.666*Solar.Radiation
+# 습도, 풍속, 일사량의 변화가 42.4%를 설명
+# 일사량이 낮을 수록 자전거 대여 갯수가 가장 크게 줄어든다
